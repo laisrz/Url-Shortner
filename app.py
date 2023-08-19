9c0ac089e923db339709aee27e514671f43aec70
Lais <laisrz@yahoo.com.br>
2023-08-19 15:52:38 -0300

    add function delete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
-from datetime import datetime
+from pony.orm import *
+from datetime import datetime, date
 import uuid
 import validators
 from validators import ValidationFailure
@@ -31,15 +32,17 @@ def shorten_url():
     '''Transform long url in short url'''
     '''Insert it into database'''
 
-    # get data from json
+    # Get data from json
     data = request.get_json()
 
     long_url = data["long_url"]
 
     expiration_date = data["expiration_date"]
 
-    ### validate the url
-
+    # Validate URL
+    result = validators.url(long_url)
+    if isinstance(result, ValidationFailure):
+        return jsonify({"message": "Invalid url"})
 
     ### algoritm to transform long url into short url
     short_url = str(uuid.uuid4())
@@ -62,7 +65,42 @@ def shorten_url():
 
 @app.route("/")
 def get_url():
-    pass
+    '''Retrieve short url'''
+    
+    # get data from json
+    data = request.get_json()
+
+    long_url = data["long_url"]
+
+    # Creation of a cursor to database
+    cursor = mysql.connection.cursor()
+
+    # Fetch id of short url to be deleted
+    id = cursor.execute("SELECT id FROM url WHERE long_url = ?", long_url)
+
+    if not id:
+        return jsonify({"message": "URL cannot be retrieved"})
+
+    # Check whether URL has been deleted
+    is_deleted = cursor.execute("SELECT is_deleted FROM url WHERE id = ?", id)
+
+    if is_deleted == 1:
+        return jsonify({"message": "URL cannot be retrieved"})
+    
+    # Check whether URL has been expired
+    expiration_date = cursor.execute("SELECT expiration_date FROM url WHERE id = ?", id)
+
+    if expiration_date < date.today():
+        return jsonify({"message": "URL has been expired"})
+
+    # Get shor_url from database
+    short_url = cursor.execute("SELECT short_url FROM url WHERE id = ?", id)
+
+    # Closing the cursor
+    cursor.close()
+
+    # Return message to the user
+    return jsonify({"short_url": short_url})
 
 
 @app.route("/", methods=['PUT'])
@@ -72,7 +110,28 @@ def update_url():
 
 @app.route("/", methods=['DELETE'])
 def delete_url():
-    pass
+    '''Delete short url'''
+    
+    # get data from json
+    data = request.get_json()
+
+    short_url = data["short_url"]
+
+    # Creation of a cursor to database
+    cursor = mysql.connection.cursor()
+
+    # Fetch id of short url to be deleted
+    id = cursor.execute("SELECT id FROM url WHERE short_url = ?", short_url)
+
+    # Delete from database
+    cursor.execute("UPDATE url SET is_deleted = ? WHERE id = ?", (1, [id]))
+    mysql.connection.commit()
+
+    # Closing the cursor
+    cursor.close()
+
+    # Return message to the user
+    return jsonify({"message": "Short URL deleted"})
 
 
 
